29b361733f11301ff806993e180d83590338b5d5
sunn9sunn9 <134864336+sunn9sunn9@users.noreply.github.com>
2023-11-08 23:52:38 +0300

    Create zadanie1.3.py

---
+++ Rabochaya_Tetrad_1_Ushakov/zadanie1.3.py
@@ -0,0 +1,7 @@
+x = 5 >= 2
+a = {1, 3, 7, 8}
+b = {2, 4, 5, 10, 'apple'}
+c = a & b
+df = 'Антонова Антонина', 34, 'ж'
+z = 'type'
+print(x, '|', type(x), '\n', a, '|', type(a), '\n', b, '|', type(b), '\n', c, '|', type(c), '\n', df, '|', type(df), '\n', z, '|', type(z))
